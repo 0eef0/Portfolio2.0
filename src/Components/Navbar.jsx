c4bbc8b62ec67facf6d72d32355ee84f280b298f
TheCapn-MEC <erolda266@west-mec.org>
2021-10-24 22:43:14 -0700

    created and worked on navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Utils/Buttons';
+
+const Navbar = () => {
+    return (
+        <div id='navbar'>
+            <div><h1>Home</h1></div>
+            <div><h1>Projects</h1></div>
+            <div><h1>Resume</h1></div>
+            <div><h1>Contact</h1></div>
+        </div>
+    )
+}
+
+export default Navbar
