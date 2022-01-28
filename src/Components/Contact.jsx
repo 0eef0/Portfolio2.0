8a1a70134f37adb62bbb29c769fc32415bfd7f89
TheCapn-MEC <erolda266@west-mec.org>
2022-01-28 16:56:34 -0700

    modified redirect. added style changes to contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -22,6 +22,7 @@ const Contact = () => {
     return (
         <div className='contactDiv'>
             <form id='emailForm' ref={form} onSubmit={sendEmail}>
+                <h1>Fill out to reach me!</h1>
                 <input type='text' placeholder='Name' id='name' name="name" style={{fontFamily: 'Arial'}} />
                 <input type='email' placeholder='Email' id='email' name="user_email" style={{fontFamily: 'Arial'}} />
                 <input type='text' placeholder='Subject' id='subject' name="subject" style={{fontFamily: 'Arial'}} />
