858d9dd574a74e7bbb552412c9d782a8a68cea79
TheCapn-MEC <erolda266@west-mec.org>
2022-05-05 16:17:16 -0700

    fixed navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom'
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
 const Navbar = () => {
-    const currPage = window.location.pathname;
+    const [currPage, setCurrPage] = useState(window.location.pathname);
 
     return (
         <div id='navbar'>
-            <div className={(currPage === '/') ? 'primary' : '' }><h1><Link to='/'>Home</Link></h1></div>
-            <div className={(currPage === '/projects') ? 'primary' : '' }><h1><Link to='/projects'>Projects</Link></h1></div>
-            <div className={(currPage === '/resume') ? 'primary' : '' }><h1><Link to='/resume'>Resume</Link></h1></div>
-            <div className={(currPage === '/contact') ? 'primary' : '' }><h1><Link to='/contact'>Contact</Link></h1></div>
+            <div onClick={() => setCurrPage(window.location.pathname)} className={(currPage === '/') ? 'primary' : '' }><h1><Link to='/'>Home</Link></h1></div>
+            <div onClick={() => setCurrPage(window.location.pathname)} className={(currPage === '/projects') ? 'primary' : '' }><h1><Link to='/projects'>Projects</Link></h1></div>
+            <div onClick={() => setCurrPage(window.location.pathname)} className={(currPage === '/resume') ? 'primary' : '' }><h1><Link to='/resume'>Resume</Link></h1></div>
+            <div onClick={() => setCurrPage(window.location.pathname)} className={(currPage === '/contact') ? 'primary' : '' }><h1><Link to='/contact'>Contact</Link></h1></div>
 
             <section id='mediaLinks'>
                 <a href='https://github.com/TheCapn-MEC' target='_blank' rel='noreferrer'><AiFillGithub /></a>
